f5f90d8fd301d5942906f3730fccc36a57931a10
ManabBiswas <156077951+ManabBiswas@users.noreply.github.com>
2024-10-14 20:26:24 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,27 @@
 document.addEventListener('DOMContentLoaded', function () {
     const addButton = document.getElementById('addButton');
     const container = document.querySelector('.container');
+    const todo = document.querySelector('.todo');
 
     addButton.addEventListener('click', addTask);
 
+    container.addEventListener('click', function (e) {
+        if (e.target && e.target.className === 'deleteButton') {
+            e.target.parentElement.remove();
+        }
+    });
+
     function addTask() {
         const taskNumber = document.querySelectorAll('.todo').length + 1;
-        
         const newTask = document.createElement('div');
         newTask.className = 'todo';
+
+        const newCheckbox = document.createElement('input');
+        newCheckbox.type = 'checkbox';
+        newCheckbox.id = `check${taskNumber}`;
+        newCheckbox.name = 'checkbox';
+        newCheckbox.className = 'todoCheckbox';
+
         const newLabel = document.createElement('label');
         newLabel.className = 'todoLabel';
         newLabel.setAttribute('for', `todo${taskNumber}`);
@@ -20,17 +33,18 @@ document.addEventListener('DOMContentLoaded', function () {
         newInput.className = 'todoValue';
         newInput.placeholder = 'Write what to do.....';
 
-        const newCheckbox = document.createElement('input');
-        newCheckbox.type = 'checkbox';
-        newCheckbox.id = `check${taskNumber}`;
-        newCheckbox.name = 'checkbox';
-        newCheckbox.className = 'todoCheckbox';
+        const deleteButton = document.createElement('button');
+        deleteButton.type = 'button';
+        deleteButton.className = 'deleteButton';
+        deleteButton.textContent = '✖';
 
+        newTask.appendChild(newCheckbox);
         newTask.appendChild(newLabel);
         newTask.appendChild(newInput);
-        newTask.appendChild(newCheckbox);
+        newTask.appendChild(deleteButton);
+
         container.appendChild(newTask);
     }
-// footer
+// footer   
     document.getElementById('current-year').textContent = new Date().getFullYear();
 });
